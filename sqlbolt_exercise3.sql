f357170244a2b5269da009ee4ab4c0df3bcb7b84
Kenan Kadic <ken.kadic@outlook.com>
2024-01-09 19:31:12 -0600

    exercise 3 initial commit

---
+++ sqlbolt_exercise3.sql
@@ -0,0 +1,11 @@
+-- Exercise 3 Tasks
+
+-- 1. Find all the Toy Story movies
+
+-- 2. Find all the movies directed by John Lasseter
+
+-- 3. Find all the movies (and director) not directed by John Lasseter
+
+-- 4. Find all the WALL-* movies
+
+
